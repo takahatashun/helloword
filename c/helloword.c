7d1e9cf01828e1cdac8d953da805684b43c946a5
takahatashun <takahatashunvn@gmail.com>
2021-11-03 22:39:39 +0700

    add c, c++

---
+++ c/helloword.c
@@ -0,0 +1,14 @@
+
+//////////////////////////////
+//
+// Author: Kazoku
+// Date: 03/11/2021
+//
+//////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+	puts("hello word");
+	return 0;
+}
